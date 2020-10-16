3c08cb035faa3c02b2dd621f0b46b4a172d0b0f6
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-10-16 14:48:07 -0600

    adding prod configuration object

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,16 @@ module.exports = {
       },
       seeds: { directory: './database/seeds' },
     },
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL,,
+      useNullAsDefault: true,
+      migrations: {
+        directory: './database/migrations',
+        tableName: 'dbmigrations',
+      },
+      seeds: { directory: './database/seeds' },
+    },
     testing: {
       client: "sqlite3",
       connection: {
