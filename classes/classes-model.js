44f351b9f1696e18e092bc301c5c52c5b072aed2
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-10-16 21:59:23 -0600

    updating insert methods to conform to postgress standards

--- classes/classes-model.js
+++ classes/classes-model.js
@@ -16,11 +16,21 @@ function findBy(filter) {
 }
 
 async function add(fitnessClass) {
-    const [id] = await db("classes").insert(fitnessClass);
+    // const [id] = await db("classes").insert(fitnessClass);
+    // uncomment for testing locally
+
+    return await db('classes').insert(fitnessClass,[
+        'id', 'class_name', 'type', 'class_time', 'duration_minutes', 'intensity_level', 'location', 'attendees', 'max_class_size', 'instructor_id'
+    ])
+    // make sure the return statement above is uncommented before pushing to prod
 
     let instructorDetails = {instructor_id: fitnessClass.instructor_id, class_id: id}
 
-    await db('instructor_classes').insert(instructorDetails)
+    // await db('instructor_classes').insert(instructorDetails)
+    // uncomment for testing locally
+    
+    return await db('instructor_classes').insert(instructorDetails)
+    // make sure the return statement above is uncommented before pushing to prod
 
     return findById(id);
 }
