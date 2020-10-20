a4da4f4eb55a2da80bf38a344084da3c517f1170
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-10-19 20:41:07 -0600

    quick code cleanup

--- classes/classes-model.js
+++ classes/classes-model.js
@@ -16,25 +16,12 @@ function findBy(filter) {
     return db("classes").where(filter);
 }
 
-// original code
-// async function add(fitnessClass) {
-//     let instructorId = fitnessClass.instructor_id;
-//     return await db('classes').insert(fitnessClass,[
-//         'id', 'class_name', 'type', 'class_time', 'duration_minutes', 'intensity_level', 'location', 'attendees', 'max_class_size', 'instructor_id'
-//     ]).then( async function relate(fitnessClass) {
-//         let instructorDetails = {instructor_id: instructorId, class_id: fitnessClass.toString()}
-//         return await db('instructor_classes').insert(instructorDetails)
-//     })
-// }
-
 async function add(fitnessClass) {
     return await db('classes').insert(fitnessClass,[
         'id', 'class_name', 'type', 'class_time', 'duration_minutes', 'intensity_level', 'location', 'attendees', 'max_class_size', 'instructor_id'
     ]).then(
         function (savedFitnessClass) {
-            console.log('savedFitnessClass from add', savedFitnessClass)
             let instructorDetails = {instructor_id: savedFitnessClass[0].instructor_id, class_id: savedFitnessClass[0].id}
-            console.log('instructor details from add', instructorDetails)
             relateInstructor(instructorDetails)
         }
     )
@@ -53,7 +40,6 @@ async function signUp(client) {
     return await db("client_classes").insert(client)
 }
 
-// 2nd called from our Add, relates the instructor to the class that was just added
 async function relateInstructor(fitnessClass) {
         return await db('instructor_classes').insert(fitnessClass)
 }
