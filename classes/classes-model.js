0c241238926b8485b470adf6c1f5e65d3c4129c2
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-10-19 20:17:17 -0600

    refacoring add again and hopefully pulling our instructor details var

--- classes/classes-model.js
+++ classes/classes-model.js
@@ -28,13 +28,12 @@ function findBy(filter) {
 // }
 
 async function add(fitnessClass) {
-    let instructorId = fitnessClass.instructor_id;
     return await db('classes').insert(fitnessClass,[
         'id', 'class_name', 'type', 'class_time', 'duration_minutes', 'intensity_level', 'location', 'attendees', 'max_class_size', 'instructor_id'
     ]).then(
         function (savedFitnessClass) {
             console.log('savedFitnessClass', savedFitnessClass)
-            let instructorDetails = {instructor_id: instructorId, class_id: savedFitnessClass[0]}
+            let instructorDetails = {instructor_id: savedFitnessClass.instructor_id, class_id: savedFitnessClass.id}
             console.log('instructor details from add', instructorDetails)
             relateInstructor(instructorDetails)
         }
